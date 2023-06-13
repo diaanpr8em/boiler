ee53d4f049712fb5a8108f2a8e40e4a4eb501928
Diaan <diaan.r8em@outlook.com>
2023-06-13 16:33:46 +0200

    insertBundle fix

--- server/db/modules/notifications.ts
+++ server/db/modules/notifications.ts
@@ -7,19 +7,22 @@ type NotificationRequest = z.TypeOf<typeof notificationBundle>;
 export const insertBundle =  (notificationBundle: NotificationRequest) => {
 	prisma.notifications.create({
 		data: {
-			entity: notificationBundle.notification.entity,
+            entity: notificationBundle.notification.entity,
+            entityId: notificationBundle.notification.entityId,
             NotificationRecipients: {
-                set [
-                    { }
-                ]
-            },
-            NotificationContent: { 
                 createMany: {
-                    data: {
-                         notificationBundle.content
-                    }
+                    data: notificationBundle.recipients
                 }
-            }
+            },
+            NotificationContent: {
+                createMany: {
+                    data: notificationBundle.content
+                }
+            },
+            type: notificationBundle.notification.type,
+            status: 'PENDING',
+            useSystemPreferences: notificationBundle.notification.useSystemPreferences,
+            userId: notificationBundle.notification.userId,
 		}
 	})
 }
