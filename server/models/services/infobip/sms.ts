115021c726ab57b1803f020e2329e1ee71dc1d0d
Michael Hanekom <mike@conac.co.za>
2023-06-25 15:21:01 +0200

    Extending email and sms pipelines

--- server/models/services/infobip/sms.ts
+++ server/models/services/infobip/sms.ts
@@ -1,45 +1,13 @@
-export class SMSAdvancedRequest {
-    messages: Message[] | undefined
-
-    constructor(data?: Message[]) {
-        if (data) {
-            Object.assign(this, data)
-            return
-        }
-
-        this.messages = []
-    }
+export type SMSAdvancedMessage = {
+    messages: Message[]
 }
-export class SMSAdvancedResponse {
 
+type Message = {
+    destinations: Destination[],
+    from: string,
+    text: string,
 }
-export class Message {
-    destinations: Destination[] | undefined
-    from: string | undefined
-    text: string | undefined
 
-    constructor(data?: Message)
-    {
-        if (data) {
-            Object.assign(this, data)
-            return
-        }
-
-        this.destinations = []
-        this.from = ""
-        this.text = ""
-    }
-}
-export class Destination {
-    to: string[] | undefined
-
-    constructor(data?: Destination)
-    {
-        if (data) {
-            Object.assign(this, data)
-            return
-        }
-
-        this.to = []
-    }
+type Destination = {
+    to: string
 }
